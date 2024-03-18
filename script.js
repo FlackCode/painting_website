e13903ed56e1e7ec71ad1003bf7857b9cd430cc0
flack <163582560+FlackCode@users.noreply.github.com>
2024-03-18 20:37:54 +0200

    Added a color picker with 4 colors!
    Unfortunately removed the color show on hover for the moment but i added 4 new colors you can select!

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 let isMouseDown = false;
+let currentColor = `black`;
 function createGridSquare(rows, columns){
     const container = document.getElementById(`grid-container`);
+
+    container.addEventListener('mouseleave', function() {
+        isMouseDown = false;
+    });
+
     for(let i = 1; i <= rows*columns; i++ ){
         const square = document.createElement(`div`);
         square.classList.add(`square`);
-        square.addEventListener('mouseover', function(){
-            if(isMouseDown){
-                square.classList.add('clicked');
+        square.addEventListener('mouseover', function () {
+            if (isMouseDown) {
+                square.style.backgroundColor = currentColor;
             }
         });
-        square.addEventListener(`mousedown`, function(){
+        square.addEventListener('mousedown', function (event) {
             event.preventDefault();
             isMouseDown = true;
-            square.classList.add('clicked');
+            square.style.backgroundColor = currentColor;
         });
-        square.addEventListener(`mouseup`, function(){
+        square.addEventListener('mouseup', function () {
             isMouseDown = false;
         });
+        document.getElementById(`red`).addEventListener(`click`, function(){
+            currentColor = `red`;
+        })
+        document.getElementById(`blue`).addEventListener(`click`, function(){
+            currentColor = `blue`;
+        })
+        document.getElementById(`green`).addEventListener(`click`, function(){
+            currentColor = `green`;
+        })
+        document.getElementById(`yellow`).addEventListener(`click`, function(){
+            currentColor = `yellow`;
+        })
+
         container.appendChild(square);
     }
 }
