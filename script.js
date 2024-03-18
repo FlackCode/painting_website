dca5bd846642e95766f65a6e36a362415581e48d
flack <163582560+FlackCode@users.noreply.github.com>
2024-03-18 19:55:24 +0200

    Added proper drawing! (Not only on click)
    Made it so you can hold to draw and removed default browser behavior during mousedown as it was causing an issue.
    Planning to add colors and to maybe add an eraser somehow(either on rightclick or eraser tool!)

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+let isMouseDown = false;
 function createGridSquare(rows, columns){
     const container = document.getElementById(`grid-container`);
     for(let i = 1; i <= rows*columns; i++ ){
         const square = document.createElement(`div`);
         square.classList.add(`square`);
-        square.addEventListener('click', function changeSquareColor(){
-            square.classList.toggle('clicked');
+        square.addEventListener('mouseover', function(){
+            if(isMouseDown){
+                square.classList.add('clicked');
+            }
+        });
+        square.addEventListener(`mousedown`, function(){
+            event.preventDefault();
+            isMouseDown = true;
+            square.classList.add('clicked');
+        });
+        square.addEventListener(`mouseup`, function(){
+            isMouseDown = false;
         });
         container.appendChild(square);
     }
